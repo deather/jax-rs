9e8c14f0e614b85d82b471baf291a3ad37677ab3
Charles Jourdan <charles.jourdan@protonmail.com>
2017-12-19 23:51:11 +0000

    Ajout de Jackson en dépendance pour gérer le json

--- src/main/java/com/skeleton/SkeletonApplication.java
+++ src/main/java/com/skeleton/SkeletonApplication.java
@@ -1,19 +1,30 @@
 package com.skeleton;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
 
 @ApplicationPath("/api")
 public class SkeletonApplication extends Application {
 
 	@Override
+	public Set<Class<?>> getClasses() {
+		Set<Class<?>> classes = new HashSet<>();
+		classes.add(JacksonJsonProvider.class);
+		classes.add(JaxbAnnotationIntrospector.class);
+		return Collections.unmodifiableSet(classes);
+	}
+
+	@Override
 	public Map<String, Object> getProperties() {
 		Map<String, Object> properties = new HashMap<>();
 		properties.put("jersey.config.server.provider.packages", this.getClass().getPackage().getName());
+		properties.put("jersey.config.disableMoxyJson", true);
+		properties.put("jersey.config.disableJsonProcessing", true);
 		return Collections.unmodifiableMap(properties);
 	}
 
--- src/main/java/com/skeleton/resources/HelloWorldResource.java
+++ src/main/java/com/skeleton/resources/HelloWorldResource.java
@@ -1,13 +1,19 @@
 package com.skeleton.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
+@Produces(MediaType.APPLICATION_JSON)
 @Path("/hello")
 public class HelloWorldResource {
 
 	@GET
-	public String hello() {
-		return "<html><body><h1>Hello World !</h1></body><html>";
+	public List<String> hello() {
+		return Arrays.asList("pouet", "tut");
 	}
 }
